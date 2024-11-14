6a1c50f70da88112f00aa573788fb88f8b6bb23d
Sanedrac2024 <167924517+Sanedrac2024@users.noreply.github.com>
2024-11-13 22:16:11 -0300

    Add files via upload

---
+++ etapa4/Form1.cs
@@ -0,0 +1,35 @@
+namespace ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = "Admin";
+            string contraseña = "Admin12345";
+            textBox2.PasswordChar = '#';
+            if (textBox1.Text == usuario || textBox1.Text == contraseña)
+            {
+                MessageBox.Show("Autorizado a ingresar", "Ingresaste");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta", "Error");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
