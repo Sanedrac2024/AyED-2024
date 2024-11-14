428dff18b68244517711d6e1b1f0ad0041456e83
Sanedrac2024 <167924517+Sanedrac2024@users.noreply.github.com>
2024-11-13 22:07:38 -0300

    Add files via upload

---
+++ etapa4/Form1.cs
@@ -0,0 +1,51 @@
+namespace ejercicio0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            k.Text = (int.Parse(textBox1.Text) + 273).ToString();
+            F.Text = (int.Parse(textBox1.Text) * 18 / 10 + 32).ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
